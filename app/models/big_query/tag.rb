b4b2566241ebd4e2ee88a177f2701ef819d9f8ad
Albert Yi <ayi@desk.com>
2016-09-20 16:45:01 -0700

    add tag model for bigquery

--- app/models/big_query/tag.rb
+++ app/models/big_query/tag.rb
@@ -9,19 +9,23 @@ module BigQuery
       updated_at: {type: "TIMESTAMP"}
     }
 
-    def self.export
-      client.delete_table("tags")
+    def export!
+      begin
+        client.delete_table("tags")
+      rescue Google::Apis::ClientError
+      end
+      
       client.create_table("tags", SCHEMA)
 
       ::Tag.find_in_batches do |group|
         batch = group.map do |x| 
           {
-            "id" => tag.id,
-            "name" => tag.name,
-            "count" => tag.post_count,
-            "category" => tag.category,
-            "created_at" => tag.created_at,
-            "updated_at" => tag.updated_at
+            "id" => x.id,
+            "name" => x.name,
+            "count" => x.post_count,
+            "category" => x.category,
+            "created_at" => x.created_at,
+            "updated_at" => x.updated_at
           }
         end
 
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,7 @@
 every 1.day do
   runner "HitCounter.new.prune!"
 end
+
+every 1.week do
+  runner "BigQuery::Tag.new.export!"
+end
