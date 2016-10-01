4421ea562e44a41595a99e1aee25500cc3791b80
r888888888 <r888888888@gmail.com>
2016-10-01 01:32:04 -0700

    include report generator in schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,10 @@ every 1.day do
   runner "HitCounter.new.prune!"
 end
 
-every 1.week do
+every :sunday, :at => "1:00 am" do
   runner "BigQuery::Tag.new.export!"
+end
+
+every :sunday, :at => "2:00 am" do
+  runner "Reports::PostChanges.new.generate"
 end
