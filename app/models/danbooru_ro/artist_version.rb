29c39b9873044ff67341227fd482869520a87515
Albert Yi <ayi@desk.com>
2016-10-19 15:07:32 -0700

    add artist export

--- app/models/danbooru_ro/artist_version.rb
+++ app/models/danbooru_ro/artist_version.rb
@@ -1,7 +1,5 @@
 module DanbooruRo
 	class ArtistVersion < Base
     attr_readonly *column_names
-
-    
 	end
 end
--- script/services/danbooru_exporter.rb
+++ script/services/danbooru_exporter.rb
@@ -55,6 +55,122 @@ Signal.trap("TERM") do
   $running = false
 end
 
+class ArtistVersionExporter
+  BATCH_SIZE = 1000
+  SCHEMA = {
+    version_id: {type: "INTEGER"},
+    version: {type: "INTEGER"},
+    created_at: {type: "TIMESTAMP"},
+    updated_at: {type: "TIMESTAMP"},
+    artist_id: {type: "INTEGER"},
+    name: {type: "STRING"},
+    updater_ip_addr: {type: "STRING"},
+    is_active: {type: "BOOLEAN"},
+    other_names: {type: "STRING"},
+    group_name: {type: "STRING"},
+    url_string: {type: "STRING"},
+    is_banned: {type: "BOOLEAN"}
+  }
+
+  attr_reader :redis, :logger, :gbq
+
+  def initialize(redis, logger, gbq)
+    @redis = redis
+    @logger = logger
+    @gbq = gbq
+  end
+
+  def get_last_exported_id
+    redis.get("artist-version-exporter-id").to_i
+  end
+
+  def find_previous(version)
+    DanbooruRo::ArtistVersion.where("artist_id = ? and updated_at < ?", version.artist_id, version.updated_at).order("updated_at desc, id desc").first
+  end
+
+  def find_version_number(version)
+    1 + DanbooruRo::ArtistVersion.where("artist_id = ? and updated_at < ?", version.artist_id, version.updated_at).count
+  end
+
+  def calculate_diff(a, b)
+    changes = {}
+
+    if a.nil? || a.name != b.name
+      changes[:name] = b.name
+    end
+
+    if a.nil? || a.is_active != b.is_active
+      changes[:is_active] = b.is_active
+    end
+
+    if a.nil? || a.other_names != b.other_names
+      changes[:other_names] = b.other_names
+    end
+
+    if a.nil? || a.group_name != b.group_name
+      changes[:group_name] = b.group_name
+    end
+
+    if a.nil? || a.url_string != b.url_string
+      changes[:url_string] = b.url_string
+    end
+
+    if a.nil? || a.is_banned != b.is_banned
+      changes[:is_banned] = b.is_banned
+    end
+
+    return changes
+  end
+
+  def create_table
+    begin
+      gbq.create_table("artist_versions", SCHEMA)
+    rescue Google::Apis::ClientError
+    end
+  end
+
+  def execute
+    create_table
+
+    begin
+      last_id = get_last_exported_id
+      next_id = last_id + BATCH_SIZE
+      store_id = last_id
+      batch = []
+      DanbooruRo::ArtistVersion.where("id > ? and id <= ? and updated_at < ?", last_id, next_id, 70.minutes.ago).find_each do |version|
+        previous = find_previous(version)
+        diff = calculate_diff(previous, version)
+        diff[:version_id] = version.id
+        diff[:version] = find_version_number(version)
+        diff[:created_at] = version.created_at
+        diff[:updated_at] = version.updated_at
+        diff[:artist_id] = version.artist_id
+        diff[:updater_id] = version.updater_id
+        diff[:updater_ip_addr] = version.updater_ip_addr.to_s
+
+        batch << diff
+
+        if version.id > store_id
+          store_id = version.id
+        end
+      end
+
+      if batch.any?
+        logger.info "inserting #{last_id}..#{store_id}"
+        result = gbq.insert("artist_versions", batch)
+        if result["insertErrors"]
+          logger.error result.inspect
+        else
+          redis.set("artist-version-exporter-id", store_id)
+        end
+      end
+
+    rescue Exception => e
+      logger.error "error: #{e}"
+    end
+  end
+end
+
 class NoteExporter
   BATCH_SIZE = 1000
   SCHEMA = {
@@ -159,18 +275,13 @@ class NoteExporter
         result = gbq.insert("note_versions_flat", batch)
         if result["insertErrors"]
           logger.error result.inspect
-          sleep(180)
         else
           redis.set("flat-note-version-exporter-id", store_id)
         end
-      else
-        sleep(60)
       end
 
     rescue Exception => e
       logger.error "error: #{e}"
-      sleep(60)
-      retry
     end
   end
 end
@@ -250,6 +361,8 @@ class FlatPostVersionExporter
   end
 
   def execute
+    create_table
+    
     begin
       last_id = get_last_exported_id
       next_id = last_id + BATCH_SIZE
@@ -308,18 +421,13 @@ class FlatPostVersionExporter
         result = gbq.insert("post_versions_flat", batch)
         if result["insertErrors"]
           logger.error result.inspect
-          sleep(180)
         else
           redis.set("flat-post-version-exporter-id", store_id)
         end
-      else
-        sleep(60)
       end
 
     rescue Exception => e
       logger.error "error: #{e}"
-      sleep(60)
-      retry
     end
   end
 end
@@ -405,18 +513,13 @@ class PostVersionExporter
         result = GBQ.insert("post_versions", batch)
         if result["insertErrors"]
           logger.error result.inspect
-          sleep(180)
         else
           redis.set("post-version-exporter-id", store_id)
         end
-      else
-        sleep(60)
       end
 
     rescue Exception => e
       logger.error "error: #{e}"
-      sleep(60)
-      retry
     end
   end
 end
@@ -492,6 +595,8 @@ class WikiPageExporter
 
   def execute
     begin
+      create_table
+
       last_id = get_last_exported_id
       next_id = last_id + BATCH_SIZE
       store_id = last_id
@@ -518,28 +623,31 @@ class WikiPageExporter
         result = GBQ.insert("wiki_page_versions", batch)
         if result["insertErrors"]
           logger.error result.inspect
-          sleep(180)
         else
           redis.set("wiki-exporter-id", store_id)
         end
-      else
-        sleep(60)
       end
 
     rescue Exception => e
       logger.error "error: #{e}"
-      sleep(60)
-      retry
     end
   end
 end
 
 while $running
   NoteExporter.new(REDIS, LOGGER, GBQ).execute
-  break unless $running
+  exit unless $running
   FlatPostVersionExporter.new(REDIS, LOGGER, GBQ).execute
-  break unless $running
+  exit unless $running
   PostVersionExporter.new(REDIS, LOGGER, GBQ).execute
-  break unless $running
+  exit unless $running
   WikiPageExporter.new(REDIS, LOGGER, GBQ).execute
+  exit unless $running
+  ArtistVersionExporter.new(REDIS, LOGGER, GBQ).execute
+  exit unless $running
+
+  60.times do
+    sleep(1)
+    exit unless $running
+  end
 end
