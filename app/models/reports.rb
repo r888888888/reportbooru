2377872c793509d6930de76feb3974c0c1651337
Albert Yi <ayi@desk.com>
2016-10-28 11:20:47 -0700

    include additional reports in generate_all

--- app/models/reports.rb
+++ app/models/reports.rb
@@ -1,6 +1,6 @@
 module Reports
   def generate_all
-    [Artists, BulkUpdateRequests, Comments, ContributorUploads, ForumPosts, ForumTopics MemberUploads, PostAppeals, PostChanges, PostFlags, Notes, TagAliases, TagImplications, Taggers, WikiPages].each do |model|
+    [ArtistCommentaries, Artists, BulkUpdateRequests, Comments, ContributorUploads, ForumPosts, ForumTopics MemberUploads, PostAppeals, PostChanges, PostFlags, Notes, TagAliases, TagImplications, Taggers, WikiPages].each do |model|
       model.new.generate
     end
   end
