eb3e0b969992ac1fab6a251fca0ec9f79b5471af
r888888888 <r888888888@gmail.com>
2016-12-25 11:11:11 -0800

    move directory to user-reports

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,6 @@ set :default_env, {
   "PATH" => '$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH',
   "RAILS_ENV" => "production"
 }
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'public/reports', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'public/user-reports', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
 set :linked_files, fetch(:linked_files, []).push(".env")
 set :rbenv_ruby, "2.3.1"
