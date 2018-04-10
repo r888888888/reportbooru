4dcc52366ef7ee6f4755f911dc5bd5a38ea4c7f7
Albert Yi <albert@carbonfive.com>
2018-04-10 13:27:46 -0700

    log unicorn errors

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -18,7 +18,7 @@ working_directory app_path
 ENV['RAILS_ENV'] = 'production'
 
 # Log everything to one file
-stderr_path "/dev/null"
+stderr_path "/var/log/reportbooru/unicorn.log"
 stdout_path "/dev/null"
 
 # Set master PID location
