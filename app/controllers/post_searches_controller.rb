507b4dcfef68a4538da73ea91a7bd00ca32991c5
r888888888 <r888888888@gmail.com>
2018-05-26 12:02:58 -0700

    fix controller

--- app/controllers/post_searches_controller.rb
+++ app/controllers/post_searches_controller.rb
@@ -5,8 +5,8 @@ class PostSearchesController < ApplicationController
   rescue_from ActiveSupport::MessageVerifier::InvalidSignature, with: :render_verification_error
 
   def create
-    if params[:msg]
-      tags, session_id = verify_msg(params[:msg])
+    if params[:sig]
+      tags, session_id = verify_msg(params[:sig])
       SearchCounter.new.count!(tags, session_id)
       render nothing: true
     else
