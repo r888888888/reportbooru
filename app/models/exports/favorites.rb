5e2642fd04943b32a4b42f757ca57a28a00b5e44
r888888888 <r888888888@gmail.com>
2018-07-21 22:05:00 -0700

    bug fixes

--- app/models/exports/favorites.rb
+++ app/models/exports/favorites.rb
@@ -1,6 +1,6 @@
 module Exports
   class Favorites
-    BATCH_SIZE = 1000
+    BATCH_SIZE = 10000
     SCHEMA = {
       id: {type: "INTEGER"},
       user_id: {type: "INTEGER"},
@@ -34,18 +34,18 @@ module Exports
         next_id = last_id + BATCH_SIZE
         store_id = last_id
         batch = []
-        post_id = DanbooruRo::Post.where("created_at > ?", 1.year.ago).minimum(:id)
-        DanbooruRo::Favorite.where("post_id >= ? and id > ? and id <= ?", post_id, last_id, next_id).find_each do |fav|
+        DanbooruRo::Favorite.where("id > ? and id <= ?", last_id, next_id).find_each do |fav|
           batch << {id: fav.id, user_id: fav.user_id, post_id: fav.post_id}
 
           if fav.id > store_id
-            store_id = vote.id
+            store_id = fav.id
           end
         end
 
         if batch.any?
           logger.info "favorite: inserting #{last_id}..#{store_id}"
-          result = gbq.insert("favorites", batch)
+          partition_timestamp = Time.now.strftime("%Y%m%d")
+          result = gbq.insert("favorites$#{partition_timestamp}", batch)
           if result["insertErrors"]
             logger.error result.inspect
           else
